baad6a94974b6732c6e350dbf4f571bb65102e4f
Geert Geerits <ggeerits@hotmail.com>
2023-09-03 11:13:51 +0200

    Update

--- CalendarEvents/MainPage.xaml.cs
+++ CalendarEvents/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 // Author ......: Geert Geerits - E-mail: geertgeerits@gmail.com
 // Copyright ...: (C) 2023-2023
 // Version .....: 1.0.2
-// Date ........: 2023-09-02 (YYYY-MM-DD)
+// Date ........: 2023-09-03 (YYYY-MM-DD)
 // Language ....: Microsoft Visual Studio 2022: .NET 7.0 MAUI C# 11.0
 // Description .: Read calendar events to share
 // Dependencies : NuGet Package: Plugin.Maui.CalendarStore by Gerald Versluis ; https://github.com/jfversluis/Plugin.Maui.CalendarStore
@@ -168,6 +168,13 @@ public partial class MainPage : ContentPage
         }
 
         lblCalendarEvents.Text = cCalendarEvents;
+    }
+
+    // Clear the calendar events.
+    private void OnClearEventsClicked(object sender, EventArgs e)
+    {
+        lblCalendarNames.Text = "";
+        lblCalendarEvents.Text = "";
     }
 
     // Copy calendar events to clipboard.
