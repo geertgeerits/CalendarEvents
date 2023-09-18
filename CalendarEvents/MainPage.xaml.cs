c633945861d54e625338139f9ba94ae3eb014e2a
Geert Geerits <ggeerits@hotmail.com>
2023-09-18 22:02:12 +0200

    Update

--- CalendarEvents/MainPage.xaml.cs
+++ CalendarEvents/MainPage.xaml.cs
@@ -5,7 +5,7 @@
 // Date ........: 2023-09-18 (YYYY-MM-DD)
 // Language ....: Microsoft Visual Studio 2022: .NET 7.0 MAUI C# 11.0
 // Description .: Read calendar events to share
-// Dependencies : NuGet Package: Plugin.Maui.CalendarStore version 1.0.0-preview4 ; https://github.com/jfversluis/Plugin.Maui.CalendarStore
+// Dependencies : NuGet Package: Plugin.Maui.CalendarStore version 1.0.0-preview5 ; https://github.com/jfversluis/Plugin.Maui.CalendarStore
 //                NuGet Package: Microsoft.AppCenter version 5.0.2 ; https://appcenter.ms/apps ; https://azure.microsoft.com/en-us/products/app-center/
 //                NuGet Package: Microsoft.AppCenter.Crashes version 5.0.2 
 // Thanks to ...: Gerald Versluis
---
+++ README.md
@@ -0,0 +1,15 @@
+Calendar Events
+_______________
+
+Retrieve and share events from local calendars.
+
+Supporting languages: Czech, Danish, Dutch, English, Finnish, French, German, Hungarian, Italian, Norwegian, Polish, Portuguese, Romanian, Spanish and Swedish.
+
+The program was tested on a Samsung Galaxy A3 and S21 phone, iPhone 7 and 14, iPad 8th generation.
+
+Link on Google Play Store:
+
+
+Link on Apple App Store:
+https://apps.apple.com/app/get-and-share-calendar-events/id6466659968
+
