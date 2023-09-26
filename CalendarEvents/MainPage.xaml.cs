f2c78ad63c7bff78f88bbb94c1c26766a2721942
geertgeerits <ggeerits@hotmail.com>
2023-09-26 12:22:01 +0200

    Update

--- CalendarEvents/MainPage.xaml.cs
+++ CalendarEvents/MainPage.xaml.cs
@@ -160,6 +160,10 @@ public partial class MainPage : ContentPage
         grdEvents.Margin = grid.Margin;
 #endif
 
+        // Permissions for CalendarRead.
+        Task<PermissionStatus> taskCalendarRead = CheckAndRequestCalendarRead();
+        Task.Delay(500).Wait();
+        
         // Get all the calendars from the device and put them in a picker.
         GetCalendars();
     }
@@ -477,5 +481,30 @@ public partial class MainPage : ContentPage
         //{
         //    Crashes.NotifyUserConfirmation(UserConfirmation.DontSend);
         //}
+    }
+
+    public async Task<PermissionStatus> CheckAndRequestCalendarRead()
+    {
+        PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.CalendarRead>();       
+        //await DisplayAlert("CheckAndRequestCalendarRead", status.ToString(), "OK");
+        
+        if (status == PermissionStatus.Granted)
+            return status;
+
+        if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+        {
+            // Prompt the user to turn on in settings
+            // On iOS once a permission has been denied it may not be requested again from the application
+            return status;
+        }
+
+        if (Permissions.ShouldShowRationale<Permissions.CalendarRead>())
+        {
+            // Prompt the user with additional information as to why the permission is needed
+        }
+
+        status = await Permissions.RequestAsync<Permissions.CalendarRead>();
+
+        return status;
     }
 }
