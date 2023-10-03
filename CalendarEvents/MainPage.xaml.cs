7b2770297c59f429d9c87d67fca880c4cf0ceb16
geertgeerits <ggeerits@hotmail.com>
2023-10-03 10:17:34 +0200

    Update

--- CalendarEvents/MainPage.xaml.cs
+++ CalendarEvents/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 // Author ......: Geert Geerits - E-mail: geertgeerits@gmail.com
 // Copyright ...: (C) 2023-2023
 // Version .....: 1.0.4
-// Date ........: 2023-10-02 (YYYY-MM-DD)
+// Date ........: 2023-10-03 (YYYY-MM-DD)
 // Language ....: Microsoft Visual Studio 2022: .NET 7.0 MAUI C# 11.0
 // Description .: Read calendar events to share
 // Dependencies : NuGet Package: Plugin.Maui.CalendarStore version 1.0.1 ; https://github.com/jfversluis/Plugin.Maui.CalendarStore
