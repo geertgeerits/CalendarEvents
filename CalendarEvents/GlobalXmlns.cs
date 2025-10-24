c20db59619ac1771d6c1dbabb8cfca77d1810455
geertgeerits <ggeerits@hotmail.com>
2025-10-24 15:14:48 +0200

    Using GlobalXmlns.cs

--- CalendarEvents/GlobalXmlns.cs
+++ CalendarEvents/GlobalXmlns.cs
@@ -1,1 +1,3 @@
 ﻿[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CalendarEvents")]
+[assembly: XmlnsDefinition("http://calendarevents/schemas/global", "CalendarEvents")]
+//[assembly: XmlnsDefinition("http://calendarevents/schemas/resx", "CalendarEvents.Resources.Languages")]
