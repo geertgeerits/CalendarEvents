9a1b32fd4ed98a0c5aba51ed986300a577649a80
geertgeerits <ggeerits@hotmail.com>
2025-12-21 09:57:59 +0100

    Update

--- CalendarEvents/MainPage.xaml.cs
+++ CalendarEvents/MainPage.xaml.cs
@@ -2,7 +2,7 @@
  * Author ......: Geert Geerits - E-mail: geertgeerits@gmail.com
  * Copyright ...: (C) 2023-2026
  * Version .....: 1.0.10
- * Date ........: 2025-12-20 (YYYY-MM-DD)
+ * Date ........: 2025-12-21 (YYYY-MM-DD)
  * Language ....: Microsoft Visual Studio 2026: .NET 10.0 MAUI C# 14.0
  * Description .: Read calendar events to share
  * Dependencies : NuGet Package: Plugin.Maui.CalendarStore version 4.0.0; https://github.com/jfversluis/Plugin.Maui.CalendarStore
@@ -55,7 +55,7 @@ namespace CalendarEvents
 
             //// Get the saved settings
             Globals.cTheme = Preferences.Default.Get("SettingTheme", "System");
-            Globals.cDateFormatSelect = Preferences.Default.Get("SettingDateFormatSelect", "SystemShort");
+            Globals.cDateFormatSelect = Preferences.Default.Get("SettingDateFormatSelect", "SystemLong");
             Globals.cAddDaysToStart = Preferences.Default.Get("SettingAddDaysToStart", "0");
             Globals.cAddDaysToEnd = Preferences.Default.Get("SettingAddDaysToEnd", "31");
             Globals.nSelectedCalendar = Preferences.Default.Get("SettingSelectedCalendar", 0);
